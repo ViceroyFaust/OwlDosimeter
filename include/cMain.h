0251ff2f1c76c346523e278982fc803d9535941f
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-04-29 19:49:55 +0300

    Create button and two test boxes as per tutorial Prior to this change, there was just an empty window.
    This change follows the tutorial further, creating a button, a text
    field, and a text box.
    
    https://www.youtube.com/watch?v=FOIbK4bJKS8

--- include/cMain.h
+++ include/cMain.h
@@ -6,6 +6,11 @@
 class cMain : public wxFrame {
 public:
     cMain();
+    ~cMain();
+
+    wxButton *m_btn1 = nullptr;
+    wxTextCtrl *m_txt1 = nullptr;
+    wxListBox *m_list1 = nullptr;
 };
 
 #endif // CMAIN_H
