ce10f8339d9efe021e51730f97e656a2b79f40c1
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-04-30 22:46:36 +0300

    Create a menu bar with file and quit Prior to this change, there was no menu bar nor an option to quit besides the exit button.
    This change creates a menu bar with file menu and a quit command

--- include/cMain.h
+++ include/cMain.h
@@ -8,10 +8,14 @@ public:
     cMain();
     ~cMain();
 
+    wxMenuBar *m_menubar;
+    wxMenu *m_file;
+
     wxButton *m_btn1;
     wxTextCtrl *m_txt1;
     wxListBox *m_list1;
 
+    void onQuit(wxCommandEvent& evt);
     void onButtonClicked(wxCommandEvent& evt);
 
     wxDECLARE_EVENT_TABLE();
--- src/cMain.cpp
+++ src/cMain.cpp
@@ -2,12 +2,23 @@
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
     EVT_BUTTON(10001,  cMain::onButtonClicked)
+    EVT_MENU(wxID_EXIT, cMain::onQuit)
 wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Asthma Counter by ViceroyFaust", wxPoint(30, 30), wxSize(800, 600)) {
+    m_menubar = new wxMenuBar;
+    m_file = new wxMenu;
+    m_menubar->Append(m_file, wxT("&File"));
+    m_file->Append(wxID_EXIT, wxT("&Quit"));
+    SetMenuBar(m_menubar);
+
     m_btn1 = new wxButton(this, 10001, "Click Me", wxPoint(10, 10), wxSize(150, 50));
     m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
     m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+}
+
+void cMain::onQuit(wxCommandEvent& evt) {
+    Close(true);
 }
 
 void cMain::onButtonClicked(wxCommandEvent &evt) {
